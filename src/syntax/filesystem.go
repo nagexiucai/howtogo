3c24e62c887393cc3f85e724bddb0cfa5a5ff3eb
nagexiucai <nagexiucai@qq.com>
2018-12-06 21:59:20 +0800

    继续Go基础。

--- src/syntax/filesystem.go
+++ src/syntax/filesystem.go
@@ -1,7 +1,49 @@
 package main
 
+import (
+	"os"
+	"fmt"
+	"archive/zip"
+	"io"
+	)
+
 // 压缩ZIP
-func Compress(src []string, zip string) bool {
+func Compress(src []string, zippath string) bool {
+	flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+	zipfile, err := os.OpenFile(zippath, flags, 0644)
+	defer zipfile.Close()
+	if err != nil {
+		fmt.Println("不能打开ZIP文件：", zippath)
+		return false
+	}
+	zipwtr := zip.NewWriter(zipfile)
+	defer zipwtr.Close()
+	for i:=0; i<len(src); i++ {
+		stat, err := os.Stat(src[i])
+		if err != nil {
+			fmt.Println("无法获取文件状态：", src[i])
+			continue
+		}
+		if stat.IsDir() {
+			fmt.Println("是目录：", src[i])
+			continue
+		}
+		f, err := os.Open(src[i])
+		if err != nil {
+			fmt.Println("没能打开文件：", src[i])
+			continue
+		}
+		defer f.Close()
+		wtr, err := zipwtr.Create(src[i])
+		if err != nil {
+			fmt.Println("注册ZIP失败：", zippath, src[i])
+		}else {
+			_, err := io.Copy(wtr, f)
+			if err != nil {
+				fmt.Println("写入ZIP失败：", zippath, src[i])
+			}
+		}
+	}
 	return true
 }
 // 解读ZIP
@@ -13,4 +55,20 @@ func Uncompress(zip string, dst string) bool {
 	return true
 }
 
-func main() {}
+func main() {
+	fmt.Println("当前工作目录：", os.Args[0])
+	args := os.Args[1:]
+	for i:=0; i<len(args); i++ {
+		fmt.Println("第",i,"个参数是：",args[i])
+	}
+
+	f, err := os.Create("./x.test")
+	if err != nil {
+		fmt.Println("无法创建文件：","./x.test")
+	}else{
+		defer f.Close()
+		f.Write([]byte("abcde"))
+	}
+
+	Compress([]string{".","./none.test","./x.test","./y.test","./z.test"}, "./filesystem.go.zip")
+}
