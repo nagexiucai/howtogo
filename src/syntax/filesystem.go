af50c845e8476144552bc87fe851efd1e78a6af5
nagexiucai <nagexiucai@qq.com>
2018-12-07 19:24:06 +0800

    继续GoLang基础，打点儿密码学的野。

--- src/syntax/filesystem.go
+++ src/syntax/filesystem.go
@@ -5,7 +5,12 @@ import (
 	"fmt"
 	"archive/zip"
 	"io"
-	)
+	"path"
+	"bufio"
+	"strings"
+	"log"
+	"path/filepath"
+)
 
 // 压缩ZIP
 func Compress(src []string, zippath string) bool {
@@ -34,7 +39,7 @@ func Compress(src []string, zippath string) bool {
 			continue
 		}
 		defer f.Close()
-		wtr, err := zipwtr.Create(src[i])
+		wtr, err := zipwtr.Create(path.Base(src[i]))
 		if err != nil {
 			fmt.Println("注册ZIP失败：", zippath, src[i])
 		}else {
@@ -47,11 +52,73 @@ func Compress(src []string, zippath string) bool {
 	return true
 }
 // 解读ZIP
-func List(zip string) []string {
-	return []string{}
+func List(zippath string) []string {
+	result := []string{}
+	stat, err := os.Stat(zippath)
+	if err != nil {
+		fmt.Println("无法获取文件状态：",zippath)
+		return result
+	}
+	if stat.IsDir() {
+		fmt.Println("是目录：",zippath)
+		return result
+	}
+	zipfile, err := zip.OpenReader(zippath)
+	if err == nil {
+		defer zipfile.Close()
+		for i, innerfile := range zipfile.File {
+			fmt.Println(i,"-----",innerfile.Name)
+			result = append(result, innerfile.Name)
+			f, err := innerfile.Open()
+			if err == nil {
+				defer f.Close()
+				buf := []byte{}
+				f.Read(buf) // TODO: 没读出来？
+				raw := string(buf)
+				data := bufio.NewScanner(strings.NewReader(raw))
+				data.Split(bufio.ScanRunes)
+				for data.Scan() {
+					fmt.Print(data.Text())
+				}
+			}else{
+				fmt.Println("打开内部归档文件失败：", innerfile.Name)
+			}
+		}
+	}else{
+		fmt.Println("打开ZIP失败：",zippath)
+	}
+	return result
 }
 // 解压ZIP
-func Uncompress(zip string, dst string) bool {
+func Uncompress(zippath string, dst string) bool {
+	zipfile, err := zip.OpenReader(zippath)
+	if err != nil {
+		fmt.Println("打开ZIP失败：",zippath)
+	}else{
+		defer zipfile.Close()
+		for _, innerfile := range zipfile.Reader.File {
+			zippedfile, err := innerfile.Open()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			defer zippedfile.Close()
+
+			tgt := filepath.Join(dst,path.Base(innerfile.Name))
+			if innerfile.FileInfo().IsDir() {
+				os.MkdirAll(tgt, innerfile.Mode())
+			}else{
+				outputfile, err := os.OpenFile(tgt,os.O_WRONLY|os.O_CREATE|os.O_TRUNC,innerfile.Mode())
+				if err != nil {
+					log.Fatalln(err)
+				}
+				defer outputfile.Close()
+				_, err =io.Copy(outputfile, zippedfile)
+				if err != nil {
+					log.Fatalln(err)
+				}
+			}
+		}
+	}
 	return true
 }
 
@@ -68,7 +135,12 @@ func main() {
 	}else{
 		defer f.Close()
 		f.Write([]byte("abcde"))
+		f.Write([]byte{'f','g','h','i','j'})
 	}
 
 	Compress([]string{".","./none.test","./x.test","./y.test","./z.test"}, "./filesystem.go.zip")
+
+	List("./filesystem.go.zip")
+
+	Uncompress("./filesystem.go.zip", ".")
 }
